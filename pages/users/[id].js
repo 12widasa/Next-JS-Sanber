c88a58e80add54c0f023e0dad9be7932dcad7277
12widasa <widasaputra1@gmail.com>
2023-10-11 14:40:26 +0700

    tugas hari-8

---
+++ pages/users/[id].js
@@ -0,0 +1,14 @@
+import Layout from "@/components/layout";
+import { useRouter } from "next/router";
+
+export default function UsersByName() {
+
+  const router = useRouter();
+  const { id } = router?.query;
+
+  return (
+    <Layout>
+      <p>Users By {id}</p>
+    </Layout>
+  )
+}
