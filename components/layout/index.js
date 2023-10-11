c819469a4531fc84ac39d2be59a36bbe8e7d3dde
12widasa <widasaputra1@gmail.com>
2023-10-11 07:30:22 +0700

    tugas hari-7

--- components/layout/index.js
+++ components/layout/index.js
@@ -132,6 +132,7 @@ export default function Layout({ children, metaTitle, metaDescription }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {children}
+
     </div>
   );
 }
---
+++ pages/users/detail.js
@@ -0,0 +1,12 @@
+import Layout from "@/components/layout"
+
+export default function UserDetail() {
+  return (
+
+    <div>
+      <Layout>
+        <p>User Detail</p>
+      </Layout>
+    </div>
+  )
+}
